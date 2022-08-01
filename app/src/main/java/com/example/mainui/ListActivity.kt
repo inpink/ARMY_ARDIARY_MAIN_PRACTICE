8e8c863e5b485fcd252339d6e94066c93f226d06
BuildTools <unconfigured@null.spigotmc.org>
2022-08-01 22:55:22 +0900

    Third Commit

--- app/src/main/java/com/example/mainui/ListActivity.kt
+++ app/src/main/java/com/example/mainui/ListActivity.kt
@@ -2,6 +2,16 @@ package com.example.mainui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton
+
+
+class MyEventHandler : CompoundButton.OnCheckedChangeListener{
+    override fun onCheckedChanged(p0: CompoundButton?, p1: Boolean) {
+        Log.d("na","checkbox click")
+    }
+}
+
 
 class ListActivity : AppCompatActivity(){
 
