d6450b77bc3f7f34eb52902d68b41552d0ffd4a2
inpink <inpink@kakao.com>
2022-08-28 19:23:26 +0900

    http통신 - volley (error)

--- app/src/main/java/com/example/mainui/MainActivity.kt
+++ app/src/main/java/com/example/mainui/MainActivity.kt
@@ -7,11 +7,19 @@ import android.content.Intent
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.navi_header.*
+import org.json.JSONArray
+import org.json.JSONObject
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +30,48 @@ class MainActivity : AppCompatActivity() {
 
         var my_btn : ImageButton = findViewById<ImageButton>(R.id.my_diary_btn)
         var map_btn : ImageView = findViewById<ImageView>(R.id.Button11)
+
+
+       /* // 문자열 요청
+       val url: String = "http://3.39.158.43:8088/"
+
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            Response.Listener<String> {
+                Log.d("kkang", "server data : $it")
+            },
+            Response.ErrorListener { error ->
+                Log.d("kkang", "error......$error")
+            })
+        val queue = Volley.newRequestQueue(this)
+        queue.add(stringRequest)
+*/
+
+        //json 배열 요청
+        val url: String = "http://3.39.158.43:8088/"
+
+
+        val jsonArrayRequest = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener<JSONArray> { response ->
+                Log.d("kkang","success!!! $response.length()")
+                /*for (i in 0 until response.length()) {
+                    val jsonObject = response[i] as JSONObject
+                    val title = jsonObject.getString("title")
+                    val date = jsonObject.getString("date")
+                    Log.d("kkang", "$title, $date")
+                }*/
+            },
+            Response.ErrorListener { error -> Log.d("kkang", "error......$error") }
+        )
+
+        val queue = Volley.newRequestQueue(this)
+        queue.add(jsonArrayRequest)
+
 
 
         my_btn.setOnClickListener {
@@ -39,6 +89,12 @@ class MainActivity : AppCompatActivity() {
         camera_btn.setOnClickListener{
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
+        }
+
+        right_btn.setOnClickListener{
+            thread(start=true){
+
+            }
         }
 
 
